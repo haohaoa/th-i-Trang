82590675f1787ea473cc5a03a93960f15256aa85
haohaoa <haohao051103@gmail.com>
2024-10-02 21:55:33 +0700

    ThoiTrang

---
+++ ThoiTrang/ViewModel/ShopVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ThoiTrang.Models;
+
+namespace ThoiTrang.ViewModel
+{
+    public class ShopVM
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+    public class category
+    {
+        public List<category> Categories{ get; set; }
+        public List<Product> products { get; set; } 
+    }
+    public class CartItemViewModel
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
